908b4c56435c1967273db0ceb8290934c333f8bb
HardcoreHacker <hardcorehacker112@hotmail.com>
2013-03-06 21:38:49 +0100

    Update uberizer.sh
    added RAW root commands from uberizer-windows plus the busybox menu and installer, which shall be combined with ZDiR soon!

--- uberizer.sh
+++ uberizer.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+adb wait-for-device;
 while :
 do
 clear
 echo "##########################################";
-echo "## Uberizer-linux ## by HcH / ... / ... ##";
+echo "## Uberizer-linux ##  by HcH / skrzyp   ##";
 echo "##########################################";
 echo "## a. Manage Device   ##";
 echo "## b. Root Options    ##";
-echo "## c. ROM Kitchens    ##";
+echo "## c. BusyBox menu    ##";
+echo "## d. ROM Kitchens    ##";
 echo "## q. Quit            ##";
 echo "########################";
 read choice
@@ -31,11 +33,125 @@ clear
 echo "##########################################";
 echo "## Uberizer-linux ## Root Options       ##";
 echo "##########################################";
+echo "## Root Android 1.6 Donut v1            ##";
+echo "## Root Android 1.6 Donut v2            ##";
+echo "## Root Android 2.2 Froyo               ##";
+echo "## Root Android 2.3 GingerBread         ##";
+echo "## Root Android 4.0 Ice Cream Sandwich  ##";
+echo "##########################################";
 exit 0 ;;
 read choice
 case $choice in
 
+
+:root16
+echo "Root for Android 1.6 Donut by HcH"
+echo "using Magic Android Apps su bin (no su elevation)"
+echo "Remount FS rw ..."
+remount
+echo "Copying Superuser bin ..."
+adb push changer_files/su7/su /system/bin/su
+adb push changer_files/su7/su /system/xbin/su
+echo "Setting owner ..."
+adb shell chown root.root /system/bin/su
+adb shell chown root.root /system/xbin/su
+echo "Setting permissions ..."
+adb shell chmod 4777 /system/bin/su
+adb shell chmod 4777 /system/xbin/su
+
+:root162
+echo "Root for Android 1.6 Donut v2 by HcH"
+echo "Remount FS rw ..."
+adb remount
+echo "Copying Superuser to system app ..."
+adb push changer_files/su5/Superuser.apk /system/app/Superuser.apk
+echo "Copying Superuser bin ..."
+adb push changer_files/su5/su /system/bin/su
+echo "Setting owner ..."
+adb shell chown root.root /system/bin/su
+adb shell chown root.root /system/xbin/su
+echo "Setting permissions ..."
+adb shell chmod 4777 /system/bin/su
+adb shell chmod 4777 /system/xbin/su
+
+:root23
+echo "Root for Android 2.3 GingerBread by HcH"
+echo "Remount FS rw ..."
+adb remount
+echo "Copying Superuser to system app ..."
+adb push changer_files/su8/Superuser.apk /system/app/Superuser.apk
+echo "Copying Superuser bin ..."
+adb push changer_files/su8/su /system/bin/su
+adb push changer_files/su8/su /system/xbin/su
+echo "Setting owner ..."
+adb shell chown root.root /system/bin/su
+adb shell chown root.root /system/xbin/su
+echo "Setting permissions ..."
+adb shell chmod 6755 /system/bin/su
+adb shell chmod 6755 /system/xbin/su
+adb shell chmod 755 /system/app/Superuser.apk
+echo.
+
+:root40
+echo "Root for Android 4.0.x by HcH"
+echo "Remount FS rw ..."
+adb remount
+echo "When remount FS is not enough, remounting rootfs ..."
+adb shell "mount -o rw,remount -t rootfs rootfs /"
+echo "Remounting system ..."
+adb shell "mount -o rw,remount -t /system /system"
+echo "Copying Busybox xbin ..."
+adb push changer_files/su11/busybox /system/xbin/busybox
+echo "Installing busybox ..."
+adb shell "chmod 4755 /system/xbin/busybox"
+adb shell "/system/xbin/busybox --install -s /system/xbin"
+echo "Copying SU xbin ..."
+adb push changer_files/su11/su /system/xbin/su
+echo "Copying Superuser app ..."
+adb push changer_files/su11/Superuser.apk /system/app/Superuser.apk
+echo "Settings owner ..."
+adb shell "chown root.root /system/xbin/su"
+echo "Setting permissions ..."
+adb shell chmod 6755 /system/xbin/su
+adb shell chmod 644 /system/app/Superuser.apk
+echo "Creating su symlink for xbin ..."
+adb shell rm /system/bin/su
+adb shell rm /system/sbin/su
+adb shell ln -s /system/xbin/su /system/bin/su
+
+
+
 C) while :
+do
+clear
+echo "##########################################";
+echo "## Uberizer-linux ## BusyBox Installer  ##";
+echo "##########################################";
+echo "## 1. busybox-armv5l                    ##";
+echo "##  ARMv5 ARM7EJ, ARM9E, ARM10E, XScale ##";
+echo "##                                      ##";
+echo "## 2. busybox-armv6l                    ##";
+echo "##  ARMv6 ARM11                         ##";
+echo "##                                      ##";
+echo "## 3. busybox-mips                      ##";
+echo "##  MIPS based devices                  ##";
+echo "##                                      ##";
+echo "## 4. busybox-x86_64                    ##";
+echo "##  x86/x64 32bit, 64bit                ##";
+echo "##########################################";
+exit 0 ;;
+read choice
+case $choice in
+
+echo "Copying Busybox xbin ...";
+cp changer_files/su11/busybox /system/xbin/busybox;
+echo "Installing busybox ...";
+chmod 4755 /system/xbin/busybox;
+/system/xbin/busybox --install -s /system/xbin;
+
+
+
+D) while :
 do
 clear
 echo "##########################################";
